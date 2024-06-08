d7b75b2d6a6bf454a263b2d9eed0d03d5a398b59
Your Name <you@example.com>
2024-06-08 15:40:26 -0500

    added ami

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_subnet" "pub_sub" {
   vpc_id            = aws_vpc.sat_vpc.id
   availability_zone = var.availability_zone[count.index]
   cidr_block        = var.pub_sub[count.index]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
   tags = merge(var.tags,
     {
       name = "format(%-Public-Subnet-%, var.environment, count.index)"
@@ -44,7 +45,7 @@ resource "aws_subnet" "priv_sub" {
   vpc_id            = aws_vpc.sat_vpc.id
   availability_zone = var.availability_zone[count.index]
   cidr_block        = var.priv_sub[count.index]
-  
+
   tags = merge(var.tags,
     {
       name = "format(%-Private-Subnet-%, var.environment, count.index)"
@@ -152,7 +153,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -160,7 +161,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  # owners = [ "533267047415" ]
+  # owners = [ "533267047415" ] - Only put owner if you own the Image 
 }
 
 resource "aws_instance" "luc_instance" {
@@ -169,5 +170,5 @@ resource "aws_instance" "luc_instance" {
   subnet_id                   = aws_subnet.pub_sub[0].id
   key_name                    = var.key_pair
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
+  associate_public_ip_address = var.associate_public_ip_address
 }
