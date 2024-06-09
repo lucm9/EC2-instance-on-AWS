5cc557caa560cabb7a6ce26b67d651697a0e4b37
Your Name <you@example.com>
2024-06-08 20:39:12 -0500

    ip

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["71.34.21.44/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(var.tags,
